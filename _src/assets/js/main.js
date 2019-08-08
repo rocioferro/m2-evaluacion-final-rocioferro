db50eb5616e69bbf0d0fb057b067e06ba1ff7a9f
rocioferro <fer.rodriguezrocio@gmail.com>
2019-08-08 13:07:37 +0200

    make favorites array appear on dom

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -7,10 +7,46 @@ const api = 'http://api.tvmaze.com/search/shows?q=';
 const query = input.value;
 const endpoint = api + query;
 const altPicture = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+let seriesArr = [];
+const favContainer = document.querySelector('.favseries-container');
 
 function pickFavorite(event) {
   const currentSerie =event.currentTarget;
+
   currentSerie.classList.toggle('favorite');
+
+
+  if (currentSerie.classList.contains('favorite')) {
+    seriesArr = currentSerie.getAttribute('data-name');
+    favContainer.innerHTML += seriesArr;
+  }
+  else {
+    // sacar del array
+  }
+  // const seriesArr = JSON.stringify(currentSerie);
+  // console.log(seriesArr);
+
+
+  // const seriesArr = [];
+  // for (const elements of currentSerie) {
+
+  // }
+  // seriesArr = currentSerie;
+  // // let seriesArr = [];
+  // for (let i = 0; i<currentSerie.length; i++) {
+  //   seriesArr[i] = currentSerie[i].seriesArr;
+  //   console.log(seriesArr[i]);
+  // }
+
+  //   let seriesArr = [];
+//   for (let i = 0; i < currentserie.length; i++) {
+//     seriesArr[i] = currentSerie[i].seriesArr;
+//   }
+//   for (let i = 0; i < element.length; i++) {
+//     if (element[i].classList.contains('favorite') { classList.toggle('favorite') }
+
+//   }
+// }
 }
 
 function startFavorites () {
@@ -35,7 +71,7 @@ function showSerie() {
           itemImage = item.show.image.medium;
         }
         listSeries +=
-          `<li class="li-elements">
+          `<li class="li-elements" data-name="${itemName}">
              <h2 class="show-name">${itemName}</h2>
              <img class="show-img" src=${itemImage}>
            </li>`;
@@ -56,14 +92,3 @@ button.addEventListener('click', showSerie);
 
 
 
-// let liElement = [];
-
-//   let element = [];
-//   for (let i = 0; i < list.length; i++) {
-//     element[i] = list[i].element;
-//   }
-//   for (let i = 0; i < element.length; i++) {
-//     if (element[i].classList.contains('favorite') { classList.toggle('favorite') }
-
-//   }
-// }
