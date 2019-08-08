84f36fd625d1c56f0ce4e6063cea1839e4931844
rocioferro <fer.rodriguezrocio@gmail.com>
2019-08-08 10:10:03 +0200

    elements appear in dom

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,20 +5,24 @@ const button = document.querySelector('.btn');
 const list = document.querySelector('.result-list');
 const api = 'http://api.tvmaze.com/search/shows?q=';
 const query = input.value;
+const endpoint = api + query;
 
 // creamos listener para llamar al botón
 //que lo recogido en el input se pinte en consola cuando aprietas el botón
 
 function showSerie () {
-  const endpoint = api + query;
+    let listSeries = '';
   fetch(endpoint)
     .then (response => response.json())
     .then (data => {
       for (const item of data) {
-      console.log(item.show.name, item.show.image); }
+        const itemName= item.show.name;
+        const itemImage= item.show.image.medium;
+        listSeries += `<li class="li-elements><h2 class="show-name">${itemName}</h2><img class="show-img" src=${itemImage}></li>`;
+    list.innerHTML = listSeries;
+    }
+
     })
-
-
 
 
 }
