0528ae0d792633ba1b2f0b0c45ef437d3da0c939
rocioferro <fer.rodriguezrocio@gmail.com>
2019-08-09 13:39:21 +0200

    erase function working

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,67 +2,74 @@
 
 const input = document.querySelector('.input');
 const button = document.querySelector('.btn');
+const erase = document.querySelector('.erase');
 const list = document.querySelector('.result-list');
 const api = 'http://api.tvmaze.com/search/shows?q=';
 const altPicture = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
 const favContainer = document.querySelector('.favseries-container');
 let favSeries = [];
- if (localStorage.getItem('myFavSeries') !== null) {
-  favSeries = JSON.parse(localStorage.getItem('myFavSeries'));
- }
 
-if (favSeries.length > 1 ){
-   for (let i=0; i<favSeries.length; i++) {
+if (localStorage.getItem('myFavSeries') !== null) {
+  favSeries = JSON.parse(localStorage.getItem('myFavSeries'));
+}
+if (favSeries.length > 1) {
+  for (let i = 0; i < favSeries.length; i++) {
     favContainer.innerHTML += `<ul class="fav-list">
       <li class="fav-elements">
          <h3 class="fav-name">${favSeries[i].name}</h2>
-         <img class="fav-img" src=${favSeries[i].image} >
+         <img class="fav-img" src=${favSeries[i].image}>
        </li>
     </ul>`;
   }
+}
+
+function eraseSerie () {
+  localStorage.clear('myFavSeries');
+  favContainer.innerHTML = '';
+
  }
 
-
 function pickFavorite(event) {
+  eraseSerie();
   let listFavs = '';
   const currentSerie = event.currentTarget;
-  //  obtener el titulo para el array de favoritos
-  const showCurrentName = currentSerie.querySelector('.show-name').innerHTML;
-  // // obtener la foto para el array de favoritos
-  const showCurrentImage = currentSerie.querySelector('.show-img').src;
-  const myObjects = { 'name': showCurrentName, 'image': showCurrentImage };
+  const currentName = currentSerie.querySelector('.show-name').innerHTML;
+  const currentImage = currentSerie.querySelector('.show-img').src;
+  const myObjects = { 'name': currentName, 'image': currentImage };
+
 
   currentSerie.classList.toggle('favorite');
-  if (currentSerie.classList.contains('favorite')) {
-    //   // lo guardo en el array solo si no existe
-    if (favSeries.includes(myObjects) === false) {
-      favSeries.push(myObjects);
-    } }
-    for (const item of favSeries) {
-      listFavs +=
+  if (currentSerie.classList.contains('favorite') === true) {
+    favSeries.push(myObjects);
+  }
+  for (const item of favSeries) {
+    listFavs +=
       `<ul class="fav-list">
           <li class="fav-elements">
              <h3 class="fav-name">${item.name}</h2>
-             <img class="fav-img" src=${item.image} alt="${showCurrentName}">
+             <img class="fav-img" src=${item.image} alt="${currentName}">
            </li>
         </ul>`;
 
-      favContainer.innerHTML = listFavs;
+    favContainer.innerHTML = listFavs;
     }
-    // LOCALSTORAGE
-    localStorage.setItem('myFavSeries', JSON.stringify(favSeries));
-    if (favContainer.innerHTML === ''){
-      for (let i=0; i<favSeries.length; i++) {
-        favContainer.innerHTML += `<ul class="fav-list">
+
+  printFavs();
+
+}
+function printFavs () {
+  localStorage.setItem('myFavSeries', JSON.stringify(favSeries));
+  if (favContainer.innerHTML === '') {
+    for (let i = 0; i < favSeries.length; i++) {
+      favContainer.innerHTML += `<ul class="fav-list">
           <li class="fav-elements">
              <h3 class="fav-name">${favSeries[i].name}</h2>
-             <img class="fav-img" src=${favSeries[i].image} alt="${showCurrentName}">
+             <img class="fav-img" src=${favSeries[i].image} alt="${currentName}">
            </li>
         </ul>`;
-      }
-     }
+    }
   }
-//  }
+}
 function startFavorites() {
   const listElements = document.querySelectorAll('.li-elements');
 
@@ -96,13 +103,14 @@ function showSerie() {
 
         startFavorites();
 
+
       }
     });
 
 
 }
 
-
 button.addEventListener('click', showSerie);
+erase.addEventListener('click',eraseSerie);
 
 
